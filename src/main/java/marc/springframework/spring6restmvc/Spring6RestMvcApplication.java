cc98f65d50f0be2ecf9e5812307631e6f30f63f8
Bohner, Marc <marc.bohner@rafi-group.com>
2024-02-05 11:16:29 +0100

    first commit

---
+++ src/main/java/marc/springframework/spring6restmvc/Spring6RestMvcApplication.java
@@ -0,0 +1,15 @@
+package marc.springframework.spring6restmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6RestMvcApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Spring6RestMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/marc/springframework/spring6restmvc/Spring6RestMvcApplicationTests.java
@@ -0,0 +1,15 @@
+package marc.springframework.spring6restmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestMvcApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
