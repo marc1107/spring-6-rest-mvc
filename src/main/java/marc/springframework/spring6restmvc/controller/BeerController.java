29ef0b9ac2c5e8eba2ef5ad7dfe5dc667d4cde8f
Bohner, Marc <marc.bohner@rafi-group.com>
2024-02-08 11:30:19 +0100

    implemented get beer by id

--- src/main/java/marc/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/marc/springframework/spring6restmvc/controller/BeerController.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import marc.springframework.spring6restmvc.model.Beer;
 import marc.springframework.spring6restmvc.services.BeerService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,20 +15,22 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController
 {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers()
     {
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id)
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable UUID beerId)
     {
         log.debug("Get Beer by Id - in controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 }
