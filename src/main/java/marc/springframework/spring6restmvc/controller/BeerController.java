c083fd743172873d365148c49a7460996052e175
Bohner, Marc <marc.bohner@rafi-group.com>
2024-02-08 13:34:44 +0100

    implemented delete mapping for beer

--- src/main/java/marc/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/marc/springframework/spring6restmvc/controller/BeerController.java
@@ -20,6 +20,14 @@ public class BeerController
 {
     private final BeerService beerService;
 
+    @DeleteMapping("/{beerId}")
+    public ResponseEntity deleteById(@PathVariable UUID beerId)
+    {
+        beerService.deleteById(beerId);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
     @PutMapping("/{beerId}")
     public ResponseEntity updateById(@PathVariable UUID beerId,@RequestBody Beer beer)
     {
--- src/main/java/marc/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/marc/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -108,4 +108,10 @@ public class BeerServiceImpl implements BeerService
 
         beerMap.put(existing.getId(), existing);
     }
+
+    @Override
+    public void deleteById(UUID beerId)
+    {
+        beerMap.remove(beerId);
+    }
 }
