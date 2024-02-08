95607e24c4596b8bec8c4c22c67d59787bb90cf7
Bohner, Marc <marc.bohner@rafi-group.com>
2024-02-08 10:48:41 +0100

    created beer api

--- src/main/java/marc/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/marc/springframework/spring6restmvc/controller/BeerController.java
@@ -4,16 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import marc.springframework.spring6restmvc.model.Beer;
 import marc.springframework.spring6restmvc.services.BeerService;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController
 {
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers()
+    {
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id)
     {
--- src/main/java/marc/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/marc/springframework/spring6restmvc/services/BeerService.java
@@ -2,9 +2,12 @@ package marc.springframework.spring6restmvc.services;
 
 import marc.springframework.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService
 {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
 }
