060c0a045a4dc02a01794812f909b14272b15240
Bohner, Marc <marc.bohner@rafi-group.com>
2024-02-09 11:08:42 +0100

    using mockmvc to test controller methods

--- src/test/java/marc/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/marc/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -4,15 +4,14 @@ import marc.springframework.spring6restmvc.services.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.UUID;
 
 @WebMvcTest(BeerController.class)
 class BeerControllerTest
