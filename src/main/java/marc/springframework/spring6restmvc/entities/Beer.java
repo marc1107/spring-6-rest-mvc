40ac89e6f1c739945661c0f15961b0db6a894935
Bohner, Marc <marc.bohner@rafi-group.com>
2024-02-13 15:51:18 +0100

    limiting size

--- src/main/java/marc/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/marc/springframework/spring6restmvc/entities/Beer.java
@@ -3,6 +3,7 @@ package marc.springframework.spring6restmvc.entities;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import marc.springframework.spring6restmvc.model.BeerStyle;
 import org.hibernate.annotations.GenericGenerator;
@@ -30,6 +31,8 @@ public class Beer
 
     @NotNull
     @NotBlank
+    @Size(max = 50)
+    @Column(length = 50)
     private String beerName;
 
     @NotNull
@@ -37,6 +40,7 @@ public class Beer
 
     @NotNull
     @NotBlank
+    @Size(max = 255)
     private String upc;
     private Integer quantityOnHand;
 
--- src/test/java/marc/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/marc/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,5 +1,6 @@
 package marc.springframework.spring6restmvc.repositories;
 
+import jakarta.validation.ConstraintViolationException;
 import marc.springframework.spring6restmvc.entities.Beer;
 import marc.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
@@ -7,12 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepositoryTest
 {
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeerTooLong()
+    {
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My Beer 934576345634295234905326453265230456834895341603q57q356348456786456785434567")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("1234567890")
+                    .price(new java.math.BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.flush();
+        });
+    }
 
     @Test
     void testSaveBeer()
