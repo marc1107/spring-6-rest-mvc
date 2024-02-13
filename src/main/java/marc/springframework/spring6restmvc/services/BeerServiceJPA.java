d5d6e162aa97038a5e902278429f26d12eeac7e9
Bohner, Marc <marc.bohner@rafi-group.com>
2024-02-13 13:00:39 +0100

    implemented JPA Service Operations

---
+++ src/main/java/marc/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -0,0 +1,62 @@
+package marc.springframework.spring6restmvc.services;
+
+import lombok.RequiredArgsConstructor;
+import marc.springframework.spring6restmvc.mappers.BeerMapper;
+import marc.springframework.spring6restmvc.model.BeerDTO;
+import marc.springframework.spring6restmvc.repositories.BeerRepository;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService
+{
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers()
+    {
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::beerToBeerDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id)
+    {
+        return Optional.ofNullable(beerMapper.beerToBeerDto(beerRepository.findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beer)
+    {
+        return null;
+    }
+
+    @Override
+    public void updateBeerById(UUID beerId, BeerDTO beer)
+    {
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId)
+    {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beer)
+    {
+
+    }
+}
---
+++ src/main/java/marc/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -0,0 +1,62 @@
+package marc.springframework.spring6restmvc.services;
+
+import lombok.RequiredArgsConstructor;
+import marc.springframework.spring6restmvc.mappers.CustomerMapper;
+import marc.springframework.spring6restmvc.model.CustomerDTO;
+import marc.springframework.spring6restmvc.repositories.CustomerRepository;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService
+{
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public List<CustomerDTO> listCustomers()
+    {
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id)
+    {
+        return Optional.ofNullable(customerMapper.customerToCustomerDto(customerRepository.findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customer)
+    {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID customerId, CustomerDTO customer)
+    {
+
+    }
+
+    @Override
+    public void deleteById(UUID customerId)
+    {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customer)
+    {
+
+    }
+}
