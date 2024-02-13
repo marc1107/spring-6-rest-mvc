d81bc6de8dc0006295fc451900542d69156b07f1
Bohner, Marc <marc.bohner@rafi-group.com>
2024-02-13 10:15:53 +0100

    simplified exception handling

--- src/main/java/marc/springframework/spring6restmvc/controller/NotFoundException.java
+++ src/main/java/marc/springframework/spring6restmvc/controller/NotFoundException.java
@@ -1,5 +1,9 @@
 package marc.springframework.spring6restmvc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value Not Found")
 public class NotFoundException extends RuntimeException
 {
     public NotFoundException()
