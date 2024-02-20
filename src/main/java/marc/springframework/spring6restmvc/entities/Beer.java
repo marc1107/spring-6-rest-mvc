02689c86bbeb768ef4313297144b62b2c70daf67
Bohner, Marc <marc.bohner@rafi-group.com>
2024-02-20 14:10:08 +0100

    added auto create and update timestamps

--- src/main/java/marc/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/marc/springframework/spring6restmvc/entities/Beer.java
@@ -6,8 +6,10 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import marc.springframework.spring6restmvc.model.BeerStyle;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
@@ -49,6 +51,10 @@ public class Beer
 
     @NotNull
     private BigDecimal price;
+
+    @CreationTimestamp
     private LocalDateTime createdDate;
+
+    @UpdateTimestamp
     private LocalDateTime updateDate;
 }
