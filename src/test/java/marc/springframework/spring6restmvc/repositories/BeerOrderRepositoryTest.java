11555b64650493dd3037ca030a184de70af7e364
Bohner, Marc <marc.bohner@rafi-group.com>
2024-03-15 10:05:20 +0100

    implemented association helper methods in BeerOrder and Customer so beerOrderRepository.save() can be used with persistency

--- src/test/java/marc/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
+++ src/test/java/marc/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -43,7 +43,7 @@ class BeerOrderRepositoryTest
                 .customer(testCustomer)
                 .build();
 
-        BeerOrder savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder);
+        BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
 
         System.out.println(savedBeerOrder.getCustomerRef());
     }
